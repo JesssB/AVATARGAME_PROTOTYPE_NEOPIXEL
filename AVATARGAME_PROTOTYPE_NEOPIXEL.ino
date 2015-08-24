7d7d3f6985b3ff5c2289317ef96acf814e8434dc
JesssB <im.not.a.real.blue@gmail.com>
2015-08-24 16:25:46 -0400

    added "clearAll" function

--- AVATARGAME_PROTOTYPE_NEOPIXEL.ino
+++ AVATARGAME_PROTOTYPE_NEOPIXEL.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
-#define ledCount 10
+#define ledCount 4
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(ledCount, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -22,19 +22,19 @@ if (Serial.available()) { // If data is available to read,
  
  if (val == 'Q') { // If H was received
   strip.setPixelColor(0, strip.Color(255, 0, 0));
-  strip.setBrightness(10);
- strip.show();
+  strip.setBrightness(30);
+  strip.show();
  } else if (val == 'W'){
   strip.setPixelColor(1, strip.Color(255, 0, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
  }else if (val == 'E'){
   strip.setPixelColor(2, strip.Color(255, 0, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
  }else if (val == 'R'){
   strip.setPixelColor(3, strip.Color(255, 0, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
  }
 
@@ -42,21 +42,32 @@ if (Serial.available()) { // If data is available to read,
  
  if (val == 'A') { // If H was received
   strip.setPixelColor(0, strip.Color(0, 255, 0));
-  strip.setBrightness(10);
- strip.show();
+  strip.setBrightness(30);
+  strip.show();
  } else if (val == 'S'){
   strip.setPixelColor(1, strip.Color(0, 255, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
  }else if (val == 'D'){
   strip.setPixelColor(2, strip.Color(0, 255, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
  }else if (val == 'F'){
   strip.setPixelColor(3, strip.Color(0, 255, 0));
-  strip.setBrightness(10);
+  strip.setBrightness(30);
   strip.show();
+ }else if (val == 'P'){
+  clearAll();
  }
  delay(100); // Wait 100 milliseconds for next reading
  }
+
+ void clearAll(){
+  for(int i = 0; i<ledCount; i++){
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+  delay(30);
+}
+
 
